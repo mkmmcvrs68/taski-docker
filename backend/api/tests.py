373fa23be58e97a11c08460ea68df45f0424621b
Максим Козлов <mkvrs68@gmail.com>
2023-09-08 14:50:21 +0300

    v4

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,12 +1,10 @@
 from http import HTTPStatus
 
-from django.test import Client, TestCase
-
 from api import models
+from django.test import Client, TestCase
 
 
 class TaskiAPITestCase(TestCase):
-
     def setUp(self):
         self.guest_client = Client()
 
